a9f7b18fe2dcdd758dda6141ad4055952cf7cd31
Benjamin Abena <129505079+Ab3njamn@users.noreply.github.com>
2023-11-25 12:13:42 +0300

    Update script.js
    New

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // Your JavaScript code goes here
-document.body.onload = function() {
-    // Example: Change the background color after the page loads
-    document.body.style.backgroundColor = '#a0c4ff';
-};
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+
+    // Toggle navigation links on menu icon click
+    menuIcon.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+    });
+
+    // Close navigation links when clicking outside the menu on small screens
+    document.addEventListener('click', function (event) {
+        const isMenuIconClicked = menuIcon.contains(event.target);
+        const isNavLinksClicked = navLinks.contains(event.target);
+
+        if (!isMenuIconClicked && !isNavLinksClicked && window.innerWidth <= 768) {
+            navLinks.classList.remove('show');
+        }
+    });
+
+    // Close navigation links when window is resized to more than 768px
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 768) {
+            navLinks.classList.remove('show');
+        }
+    });
+});
