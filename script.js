41617efa737afdd8bc301e831f2c5ad45727a6d5
Benjamin Abena <129505079+Ab3njamn@users.noreply.github.com>
2023-11-25 09:42:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Your JavaScript code goes here
+document.body.onload = function() {
+    // Example: Change the background color after the page loads
+    document.body.style.backgroundColor = '#a0c4ff';
+};
