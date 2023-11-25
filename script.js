c87247663400154027fda1530b4b91366226a89e
Benjamin Abena <129505079+Ab3njamn@users.noreply.github.com>
2023-11-25 14:22:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// Your JavaScript code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+
+    // Toggle navigation links on menu icon click
+    menuIcon.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+    });
+
+    // Close navigation links when clicking outside the menu on small screens
+    document.addEventListener('click', function (event) {
+        const isMenuIconClicked = menuIcon.contains(event.target);
+        const isNavLinksClicked = navLinks.contains(event.target);
+
+        if (!isMenuIconClicked && !isNavLinksClicked && window.innerWidth <= 768) {
+            navLinks.classList.remove('show');
+        }
+    });
+
+    // Close navigation links when window is resized to more than 768px
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 768) {
+            navLinks.classList.remove('show');
+        }
+    });
+});
 
